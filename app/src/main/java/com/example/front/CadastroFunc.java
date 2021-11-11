7a43f82a14c12c88696c2a03c23a013c00bc8a83
PEDRO BRISQUE <pedro.brisque@etec.sp.gov.br>
2021-11-11 08:21:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/front/CadastroFunc.java
@@ -0,0 +1,14 @@
+package com.example.front;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroFunc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_func);
+    }
+}
